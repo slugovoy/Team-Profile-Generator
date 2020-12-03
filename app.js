37d6c33af445ab19cea1563e92b8bd27869ae29d
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-12-02 21:36:01 -0700

    added separate files for manager

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const questions = require("./lib/questions");
+const managerQuestions = require("./lib/managerQuestions");
 const createEmployee = require("./lib/createEmployee");
 
 const { prompt } = require("inquirer");
@@ -10,21 +11,31 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const createManager = require("./lib/createManager");
 
 const data = [];
+const manager = async () => {
+  try {
+    const managerResponse = await prompt(managerQuestions);
+    const manager = createManager(managerResponse);
+    data.push(manager);
+    employees();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const init = async () => {
+const employees = async () => {
   try {
     const response = await prompt(questions);
 
     const employee = createEmployee(response);
     data.push(employee);
 
-    if (response.again) { 
-      init();
-
+    if (response.again) {
+      employees();
     } else {
-      const template =  render(data);
+      const template = render(data);
       await writeFile(outputPath, template, (err) =>
         err ? console.log(err) : console.log("Success!")
       );
@@ -33,4 +44,5 @@ const init = async () => {
     console.log(err);
   }
 };
-init();
+manager();
+
